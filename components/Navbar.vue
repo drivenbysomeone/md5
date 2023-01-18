0ffaf786dd3894629229cacf236367f6b78d04c4
Martin Grønkjær <martin@littlegiants.dk>
2023-01-18 13:16:53 +0100

    Navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <Nav
-    class="flex transition-all text-[15px] font-medium leading-8 bg-[#ffffffcc]"
+    class="flex transition-all text-[15px] font-medium leading-8 bg-[#ffffffcc] relative lg:content-none lg:absolute lg:w-full lg:h-1.5 lg:-top-1.5 lg:left-0 lg:bg-white lg:translate-y-0 lg:transition lg:ease-out lg:duration-200"
   >
   </Nav>
 </template>
