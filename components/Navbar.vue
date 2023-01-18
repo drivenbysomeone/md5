74e0b77b215e75a84554d6bd336005132d624da1
Martin Grønkjær <martin@littlegiants.dk>
2023-01-18 14:16:14 +0100

    Navbar-in-function

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
-  <Nav
-    class="flex items-start flex-nowrap box-border text-left transition-all text-[15px] font-medium leading-8 z-1030 bg-[#ffffffcc] relative fixed top-0 left-0 right-0 lg:content-none lg:absolute lg:w-full lg:h-1.5 lg:-top-1.5 lg:left-0 lg:bg-white lg:translate-y-0 lg:transition lg:ease-out lg:duration-200"
+  <nav
+    class="flex items-start flex-nowrap box-border text-left transition-all text-[15px] font-medium leading-8 bg-[#ffffffcc] relative fixed top-0 left-0 right-0 lg:content-none lg:absolute lg:w-full lg:h-1.5 lg:-top-1.5 lg:left-0 lg:bg-white lg:translate-y-0 lg:transition lg:ease-out lg:duration-200"
   >
-  </Nav>
+    <slot />
+  </nav>
 </template>
