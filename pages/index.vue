b34ca6edda6902c420e453f07e5fdd41cecc0a7f
Martin Grønkjær <martin@littlegiants.dk>
2023-01-23 14:09:37 +0100

    before-tailwind-config

--- pages/index.vue
+++ pages/index.vue
@@ -58,55 +58,43 @@
         <Button><span class="skew-x-[40deg]">læs mere</span></Button>
       </div>
     </section>
-    <section class="pt-28 pb-28 min-w-[800px] w-full">
-      <div class="md:mx-auto w-full pr-[15px] pl-[15px] mr-auto ml-auto">
-        <div class="flex flex-wrap flex-row -mr-[15px] -ml-[15px] box-border">
-          <div
-            class="pr-6 relative leading-relaxed h-[25rem] w-1/2 float-right block pl-[15px] pt-[48px] pb-[48px]"
-          >
-            <div
-              class="absolute top-2/4 left-[56%] -translate-x-1/2 -translate-y-1/2 w-[87%] box-border"
-            >
-              <h2 class="uppercase mt-0 font-medium">
-                alt fra prototype til serieproduktion
-              </h2>
-              <p class="mt-0 mb-4 block mb-1">
-                Vi er sparingspartner fra prototype til serie produktion, og
-                vores mission er at lette vores kunders hverdag. Dette ligger vi
-                i hurtige leveringer samt kvalitets arbejde, vi står altid klar
-                til at hjælpe vores kunder med hurtige løsninger og haste
-                leveringer. Som underleverandør til mejeri- &
-                vindmølleindustrien ved vi at pris, leveringstid samt kvalitet
-                er det vigtigste, Derfor er vores svejsepersonale blevet
-                certificeret efter gældende standarder, og vi har erhvervet os
-                WPQR`r og WPS`r til en lang række svejsesøm, så vi dækker et
-                stort område indenfor rustfri rørsvejsning. MDS Stainless ApS
-                har i foråret 2016 implementeret certificeringerne DS/EN ISO
-                9001:2008 samt ISO 3834-2:2006.
-              </p>
-            </div>
-            <!-- Picture here -->
-            <div
-              class="box-border block relative h-[25rem] z-4 leading-[1.6rem] overflow-hidden left-[1200px] align-middle -top-12"
-            >
-              <a
-                href="#"
-                class="flex flex-wrap no-underline hover:no-underline text-inherit block items-end"
-              >
-                <img
-                  src="~/assets/images/mdstainless1.jpg"
-                  alt="person-welding"
-                  class="absolute -bottom-[12%] w-full"
-                />
-              </a>
-            </div>
+    <section class="pt-28 pb-28 w-full">
+      <div class="w-full flex items-center md:flex-row flex-col gap-8">
+        <div class="pl-10 leading-relaxed w-full md:w-1/2">
+          <div class="">
+            <h2 class="uppercase mt-0 font-medium">
+              alt fra prototype til serieproduktion
+            </h2>
+            <p class="mt-0 mb-4 block mb-1">
+              Vi er sparingspartner fra prototype til serie produktion, og vores
+              mission er at lette vores kunders hverdag. Dette ligger vi i
+              hurtige leveringer samt kvalitets arbejde, vi står altid klar til
+              at hjælpe vores kunder med hurtige løsninger og haste leveringer.
+              Som underleverandør til mejeri- & vindmølleindustrien ved vi at
+              pris, leveringstid samt kvalitet er det vigtigste, Derfor er vores
+              svejsepersonale blevet certificeret efter gældende standarder, og
+              vi har erhvervet os WPQR`r og WPS`r til en lang række svejsesøm,
+              så vi dækker et stort område indenfor rustfri rørsvejsning. MDS
+              Stainless ApS har i foråret 2016 implementeret certificeringerne
+              DS/EN ISO 9001:2008 samt ISO 3834-2:2006.
+            </p>
           </div>
+          <!-- Picture here -->
+        </div>
+        <div
+          class="relative h-[25rem] z-4 leading-[1.6rem] overflow-hidden w-full md:w-1/2"
+        >
+          <img
+            src="~/assets/images/mdstainless1.jpg"
+            alt="person-welding"
+            class=""
+          />
         </div>
       </div>
     </section>
     <!-- MANGLER -->
     <section
-      class="mb-14 sm:mb-28 relative h-auto max-h-[25rem] overflow-hidden z-19 object-scale-down bg-[#212a4b]"
+      class="relative h-auto max-h-[25rem] overflow-hidden z-19 object-scale-down bg-[#212a4b]"
     >
       <div class="text-center container mx-auto px-5 py-10 sm:p-28">
         <div>
@@ -114,8 +102,10 @@
             Kontakt os for at høre mere om dine muligheder
           </h2>
 
-          <div class="flex gap-8 flex-row justify-center">
-            <Button class="flex justify-center">Knap </Button>
+          <div class="flex flex-row justify-center items-center">
+            <Button class="flex justify-center"
+              ><span class="skew-x-[40deg]">Kontakt os</span>
+            </Button>
           </div>
         </div>
       </div>
@@ -144,3 +134,37 @@ export default Vue.extend({
   name: "IndexPage",
 });
 </script>
+
+<style lang="scss">
+.about {
+  overflow: hidden;
+  height: calc(400px + 14rem);
+  .about-text {
+    padding-right: 1.5rem;
+    position: relative;
+    line-height: 1.6;
+    height: 400px;
+    .wrapper {
+      position: absolute;
+      transform: translate(-50%, -50%);
+      top: 50%;
+      left: 56%;
+      width: 87%;
+    }
+    &:after {
+      display: block;
+      position: inherit;
+      height: 0;
+      width: 0;
+      content: "";
+      left: 0;
+      bottom: 0;
+      border-top: 400px solid #fff;
+      border-right: 400px solid transparent;
+      float: right;
+      margin: -48px -424px 0 0;
+      z-index: 5;
+    }
+  }
+}
+</style>
