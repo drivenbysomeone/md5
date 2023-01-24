70b37830157eed416bed3968656af3c32de1403e
Martin Grønkjær <martin@littlegiants.dk>
2023-01-24 13:13:33 +0100

    before-tailwind-module

---
+++ components/Logo.vue
@@ -0,0 +1,30 @@
+<template>
+  <picture class="NuxtLogo">
+    <source
+      :srcSet="require('~/assets/images/mdstainless-logo.png')"
+      type="image/png"
+    />
+    <source
+      :srcSet="require('~/assets/images/mdstainless-logo.png')"
+      type="image/png"
+    />
+    <img
+      :src="require('~/assets/images/mdstainless-logo.png')"
+      alt="MDS Stainless logo"
+      class="lazyload"
+    />
+  </picture>
+</template>
+
+<style>
+.NuxtLogo {
+  animation: 1s appear;
+  margin: auto;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,14 +1,20 @@
 <template>
   <nav fixed="top" toggleable="lg" type="dark" :class="{ scrolled }">
-    dfdfd
     <!-- NOTE lg is 1024px and NOT the required 991px - customize this! -->
     <div
       class="pl-[100px] lg:bg-[#212a4be6] -lg:mt-[9px] lg:w-full lg:py-0 px-4 -lg:mt-0 lg:z-199 lg:border-transparent lg:border-none lg:bg-transparent lg:after:hidden"
     >
       <!-- HJÆLP Collapse i div? -->
-      <div class="collapse bg-[#212a4ae6] z-10"></div>
-      dfd
+      <img
+        src="~/assets/images/mdstainless-logo.png"
+        alt="mds logo"
+        class="m-auto NuxtLogo"
+      />
+      <div
+        class="collapse bg-[#212a4ae6] z-10 pt-[50px] pb-2.5 pb h-auto"
+      ></div>
     </div>
+    <slot />
   </nav>
 </template>
 
