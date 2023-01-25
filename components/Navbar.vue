c7dccbed021b2a9a8aa69d905f7c410a5b41fea0
Martin Grønkjær <martin@littlegiants.dk>
2023-01-25 14:36:19 +0100

    25-01

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,23 +1,213 @@
 <template>
-  <nav fixed="top" toggleable="lg" type="dark" :class="{ scrolled }">
+  <nav
+    fixed="top"
+    toggleable="lg"
+    type="dark"
+    class="flex"
+    :class="{ scrolled }"
+  >
+    <Logo />
     <!-- NOTE lg is 1024px and NOT the required 991px - customize this! -->
     <div
-      class="pl-[100px] lg:bg-[#212a4be6] -lg:mt-[9px] lg:w-full lg:py-0 px-4 -lg:mt-0 lg:z-199 lg:border-transparent lg:border-none lg:bg-transparent lg:after:hidden"
+      class="flex pl-[100px] lg:bg-[#212a4be6] -lg:mt-[9px] lg:w-full lg:py-0 px-4 -lg:mt-0 lg:z-199 lg:border-transparent lg:border-none lg:bg-transparent lg:after:hidden justify-between items-center"
     >
       <!-- HJÆLP Collapse i div? -->
-      <img
-        src="~/assets/images/mdstainless-logo.png"
-        alt="mds logo"
-        class="m-auto NuxtLogo"
-      />
+
       <div
         class="collapse bg-[#212a4ae6] z-10 pt-[50px] pb-2.5 pb h-auto"
       ></div>
     </div>
-    <slot />
   </nav>
 </template>
 
-<!-- <script lang="ts">
-import { Component, Prop, Vue } from "nuxt-property-decorator";
-</script> -->
+<style>
+nav {
+  transition: all 1s ease-out;
+  height: 60px;
+  line-height: 60px;
+}
+nav .nav-container {
+  padding-left: 100px;
+}
+nav .right-bg {
+  display: inline-block;
+  float: right;
+  padding-right: 2em;
+  position: relative;
+  z-index: 1;
+}
+nav .right-bg a {
+  color: white;
+  padding: 1em;
+  position: relative;
+  z-index: 2;
+}
+nav .right-bg:after {
+  content: "";
+  width: 1000%;
+  height: 100%;
+  background: rgba(33, 42, 75, 0.9);
+  -webkit-transform: skew(-45deg, 0deg);
+  transform: skew(-45deg, 0deg);
+  position: absolute;
+  right: -100px;
+  top: 0;
+  z-index: 0;
+}
+@media (max-width: 991px) {
+  nav {
+    padding: 0.5rem 0px !important;
+  }
+  nav .nav-container {
+    background: #212a4a e6;
+    margin-top: -9px;
+    width: 100%;
+    padding: 0rem 1rem !important;
+  }
+  nav .nav-container .collapse {
+    background: rgba(33, 42, 75, 0.2);
+    z-index: 10;
+  }
+  nav .navbar-toggler {
+    margin-top: 0px;
+    z-index: 199;
+    border-color: transparent;
+    border: none;
+  }
+  nav .nav-container.show {
+    background: transparent;
+  }
+  nav .right-bg:after {
+    display: none;
+  }
+}
+nav.scrolled .navbar-brand {
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+nav.scrolled .navbar-brand img {
+  height: 40px;
+}
+nav.scrolled .navbar-brand:after {
+  left: -120%;
+  width: 260%;
+}
+.navbar .navbar-collapse {
+  transition: all 0.2s ease-out;
+  padding-top: 0.5rem;
+  padding-bottom: 0.3125rem;
+}
+@media (max-width: 600px) {
+  .navbar .navbar-collapse a {
+    font-size: 20px !important;
+  }
+}
+.navbar-brand {
+  height: auto;
+  z-index: 100;
+  padding-top: 50px;
+  padding-bottom: 10px;
+  padding-right: 50px;
+  transition: all 1s cubic-bezier(0.25, 1, 0.5, 1);
+  position: relative;
+}
+.navbar-brand img {
+  transition: all 1s cubic-bezier(0.25, 1, 0.5, 1);
+  height: 80px;
+}
+.navbar-brand:after {
+  content: "";
+  width: 260%;
+  height: 100%;
+  background: rgba(255, 255, 255, 1);
+  transform: skew(-45deg, 0deg);
+  position: absolute;
+  left: -120%;
+  top: 0;
+  z-index: -10;
+}
+@media (max-width: 1200px) {
+  .navbar-brand {
+    padding-top: 10px;
+    padding-bottom: 10px;
+  }
+  .navbar-brand img {
+    height: 40px;
+  }
+  .navbar-brand:after {
+    left: -120%;
+    width: 260%;
+  }
+}
+.nav-item {
+  padding: 0 1.2rem;
+}
+.navbar a.nav-link {
+  transition: all 1s cubic-bezier(0.25, 1, 0.5, 1);
+  font-size: 15px;
+  font-weight: 500;
+  line-height: 32px;
+  color: rgba(255, 255, 255, 0.8) !important;
+  position: relative;
+  display: block;
+}
+@media (min-width: 991px) {
+  .navbar a.nav-link:after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 5px;
+    top: -6px;
+    left: 0;
+    background: white;
+    transform: translateY(0px);
+    transition: transform 0.2s ease-out;
+  }
+  .navbar a.nav-link:hover:after,
+  .navbar a.nav-link.active:after {
+    transform: translateY(5px);
+  }
+}
+@media (max-width: 1040px) {
+  .navbar a.nav-link {
+    font-size: 13px;
+  }
+  .navbar a.nav-link:after {
+    top: -8px;
+  }
+}
+</style>
+
+<script>
+import Logo from "~/components/Logo.vue";
+export default {
+  data() {
+    return {
+      scrolled: false,
+    };
+  },
+  mounted() {
+    if (process.client) this.handleScroll();
+  },
+
+  methods: {
+    handleScroll() {
+      this.scrolled = $nuxt.$route.name != "index" || window.scrollY > 0;
+    },
+  },
+  watch: {
+    $route(to, from) {
+      this.handleScroll();
+    },
+  },
+  created() {
+    if (process.client) window.addEventListener("scroll", this.handleScroll);
+  },
+  destroyed() {
+    if (process.client) window.removeEventListener("scroll", this.handleScroll);
+  },
+  components: {
+    Logo,
+  },
+};
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -46,6 +46,294 @@ html {
   -webkit-font-smoothing: antialiased;
 }
 
+h2,
+h1 {
+  text-transform: uppercase;
+}
+
+/* ALL BELOW has been converted from SCSS to */
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+/*** FONT STYLES ***/
+
+.text-gray {
+  color: #d1d0da;
+}
+.text-darker {
+  color: #64636b;
+}
+.text-blue {
+  color: #212a4b;
+}
+@media (max-width: 768px) {
+  h1 {
+    font-size: 1.5rem;
+  }
+  h2 {
+    font-size: 1.4rem;
+  }
+  h3 {
+    font-size: 1.2rem;
+  }
+}
+/*** BUTTON STYLES ***/
+.btn-white {
+  background: white;
+  color: #212a4b;
+  font-weight: 600;
+  text-transform: uppercase;
+  border: white solid 2px;
+  border-radius: 0;
+  margin-right: 15px;
+  -ms-transform: skewX(-40deg);
+  -webkit-transform: skewX(-40deg);
+  transform: skewX(-40deg);
+}
+.btn-white:hover {
+  border: white 2px solid;
+  background: transparent;
+  transition: 0.2s ease-in;
+}
+.btn-content {
+  -ms-transform: skewX(40deg);
+  -webkit-transform: skewX(40deg);
+  transform: skewX(40deg);
+  display: inline-block;
+}
+/*** SECTION STYLES ***/
+section.tall {
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+}
+div.tall {
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+}
+@media (min-width: 800px) {
+  section.tall {
+    padding-top: 7rem;
+    padding-bottom: 7rem;
+  }
+  div.tall {
+    padding-top: 7rem;
+    padding-bottom: 7rem;
+  }
+}
+.bg-blue {
+  background-color: #212a4b;
+}
+.bg-dark {
+  background-color: #2f2e2e !important;
+}
+.bg-light {
+  background-color: #bab8bd !important;
+}
+/***PAGES - SERVICES ***/
+.hero {
+  height: 50vh;
+  overflow: hidden;
+  position: relative;
+}
+.hero .text-wrapper {
+  position: absolute;
+  transform: translate(-50%, -50%);
+  top: 57%;
+  left: 50%;
+  z-index: 3;
+}
+.hero .text-wrapper h1 {
+  font-size: 3.5rem;
+  line-height: 1.2;
+}
+@media (max-width: 768px) {
+  .hero .text-wrapper {
+    position: relative;
+    margin-top: 30px;
+    margin-bottom: 30px;
+    top: 100px;
+    left: 0;
+    transform: translate(0, 0);
+    height: auto;
+    min-height: 400px;
+  }
+  .hero .text-wrapper h1 {
+    font-size: 2.2rem;
+  }
+}
+@media (max-width: 330px) {
+  .hero .text-wrapper {
+    top: 75px;
+  }
+  .hero .text-wrapper h1 {
+    font-size: 2rem;
+  }
+}
+.info {
+  height: auto;
+  overflow: hidden;
+}
+.info .info-text {
+  padding-right: 1.5rem;
+  position: relative;
+  line-height: 1.6;
+  height: 400px;
+}
+.info .info-text .wrapper {
+  position: absolute;
+  top: 46%;
+  left: 115px;
+  width: 70%;
+}
+.info .info-text:after {
+  display: block;
+  position: inherit;
+  height: 0;
+  width: 0;
+  content: "";
+  left: 20%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  border-top: 437px solid #212a4b;
+  border-right: 437px solid transparent;
+  float: right;
+  margin: 0px -486px 0 0;
+  z-index: 5;
+}
+@media (max-width: 1900px) {
+  .info .info-text:after {
+    margin: 0px -493px 0 0;
+  }
+}
+@media (max-width: 1770px) {
+  .info .info-text:after {
+    margin: 0px -513px 0 0;
+  }
+}
+@media (max-width: 1561px) {
+  .info .info-text:after {
+    margin: 0px -527px 0 0;
+  }
+}
+@media (max-width: 1407px) {
+  .info .info-text:after {
+    margin: 0px -537px 0 0;
+  }
+}
+@media (max-width: 1315px) {
+  .info .info-text:after {
+    margin: 0px -547px 0 0;
+  }
+}
+@media (max-width: 1219px) {
+  .info .info-text:after {
+    margin: 0px -557px 0 0;
+  }
+}
+@media (max-width: 1105px) {
+  .info .info-text:after {
+    margin: 0px -567px 0 0;
+  }
+}
+@media (max-width: 991px) {
+  .info .info-text {
+    height: auto;
+  }
+  .info .info-text .wrapper {
+    position: relative;
+    top: 100px;
+    left: 15px;
+    width: calc(100% - 30px);
+  }
+  .info .info-text .wrapper h1 {
+    font-size: 1.5rem;
+  }
+  .info .info-text .wrapper p {
+    font-size: 14px;
+    margin-bottom: 70px;
+  }
+  .info .info-text:after {
+    display: none;
+  }
+}
+.info .info-image {
+  z-index: 4;
+  height: 400px;
+  position: relative;
+}
+.info .info-image img {
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+}
+@media (max-width: 1500px) {
+  .info .info-image img {
+    width: 130%;
+  }
+}
+@media (max-width: 991px) {
+  .info .info-image {
+    height: 250px;
+  }
+  .info .info-image img {
+    width: 100%;
+    position: relative;
+  }
+}
+@media (max-width: 330px) {
+  .info .info-image {
+    height: 200px;
+  }
+}
+.info .breadcrumbs {
+  position: absolute;
+  top: 120px;
+  left: 115px;
+}
+.info .breadcrumbs img {
+  margin-top: -3px;
+}
+.info .breadcrumbs p {
+  font-weight: 600;
+  display: inline;
+  padding-left: 10px;
+}
+@media (max-width: 991px) {
+  .info .breadcrumbs {
+    top: 100px;
+    left: 15px;
+  }
+}
+@media (max-width: 768px) {
+  .info {
+    height: auto;
+  }
+}
+.content {
+  padding-left: 100px;
+  padding-right: 100px;
+}
+.content .content-text {
+  padding-right: 70px;
+}
+@media (max-width: 991px) {
+  .content {
+    padding-left: 0px;
+    padding-right: 0px;
+  }
+  .content .content-text {
+    padding-right: 15px;
+  }
+  .content .content-images {
+    padding-left: 0px;
+    padding-right: 0px;
+  }
+}
+
 /*   padding: 28px; */
 
 h2,
